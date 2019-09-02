0ae6e67ea0d0204065e7b69d2874d7a44804fe26
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-02 18:53:52 +0300

    added pause function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use std::io::prelude::*;
 
 fn main() {
     println!("Guessing game!");
@@ -27,6 +28,8 @@ fn main() {
             break;
         }
     }
+
+    pause();
 }
 
 fn number() {
@@ -114,4 +117,16 @@ fn word() {
             continue;
         }
     }
+}
+
+fn pause() {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "Press any key to exit...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
 }
