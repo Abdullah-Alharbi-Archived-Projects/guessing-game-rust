c3e823f8c5f57f63ceb26f3e2f506936a12a5479
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-02 16:04:43 +0300

    user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing game!");
+    println!("please select the game you want to play!");
+    println!("1- Guess the number\n2- Guess the word");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("Your guess is: {}", guess)
 }
