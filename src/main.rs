83153d65d95bd5ab6cd91aaf8beb1d1f23bea934
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-02 18:10:26 +0300

    implemented guess number game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -5,10 +7,58 @@ fn main() {
     println!("please select the game you want to play!");
     println!("1- Guess the number\n2- Guess the word");
 
-    let mut guess = String::new();
+    loop {
+        let mut game = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut game)
+            .expect("Failed to read line");
 
-    println!("Your guess is: {}", guess)
+        let game: u32 = match game.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if game == 1 {
+            number();
+            break;
+        } else {
+            word();
+            break;
+        }
+    }
+}
+
+fn number() {
+    println!("Guess the number started enter a number between 1 and 100 ...");
+    // generate random number between 1 and 100
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Your guess is: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
+}
+
+fn word() {
+    println!("not implemented yet.");
 }
